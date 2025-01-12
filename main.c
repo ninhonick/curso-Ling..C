90e19eca55af9a09c7eb5c07291dcbbab2886f59
ninhonick <ninhosvs@gmail.com>
2025-01-12 20:47:56 -0300

    inicio das aulas

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf ("mensagem 1. \n");
+    printf ("mensagem 2. \n");
+    printf ("mensagem 3. \n");
+    printf ("mensagem 4. \n");
+
+    return 0;
+    
+}
