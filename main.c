888d4faf0dd893f6c2ecd2e071e61c248c164770
ninhonick <ninhosvs@gmail.com>
2025-01-17 00:42:50 -0300

    descobrindo mais sobre c

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-    printf ("mensagem 1. \n");
-    printf ("mensagem 2. \n");
-    printf ("mensagem 3. \n");
-    printf ("mensagem 4. \n");
-    printf ("mensagem 5. \n");
+int main(int argc, char const *argv[]) {
+
+     char *nome = "ALEXANDRE";
+    float valor1 = 3.14159265 ;
+
+     
+    printf ("ola meu nome e : %c \n", nome);
+    printf ("valor inteiro : %d \n", 10) ;
+    printf ("valor real : %f \n", valor1);
+    printf ("valor real com apenas dua casas : %.2f. \n", valor1);
+    printf ("valor inteiro : %d \n" , valor1);
+    printf ("valor inteiro : %s \n", "testando");
+    
+    
 
     return 0;
     
