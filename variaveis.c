c0c74aa4018d228bd131e1d1b549adc11624ce87
ninhonick <ninhosvs@gmail.com>
2025-01-18 01:09:18 -0300

    introdusão

---
+++ variaveis.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int int main(int argc, char const *argv[])
+{
+   int poder = 0;
+   float nota = 0;
+   char media [10] = "Alexandre";
+
+   printf ();
+
+    return 0;
+}
