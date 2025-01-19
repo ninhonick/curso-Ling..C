3bb4cbb16f1756833ff9e8bd50136d12b81198e6
ninhonick <ninhosvs@gmail.com>
2025-01-19 02:52:41 -0300

    Aula sobre estrutura condicional if

---
+++ output/if.c
@@ -0,0 +1,18 @@
+ #include<stdio.h>
+ #include<stdlib.h>
+
+ int main(int argc, char const *argv[])
+ {
+   float m =0;
+
+   printf("INSIRA A NOTA OBTIDA \n");
+   scanf("%f",&m);
+
+   if (m >= 7.0){
+    printf("Aprovado!!");
+   }
+    r
+ }
+ 
+  
+ 
