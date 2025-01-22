fe764067eeb19ec9506e55ece50180cc38b85667
ninhonick <ninhosvs@gmail.com>
2025-01-22 01:05:46 -0300

    inicio projeto tabuada

--- wlile.c
+++ wlile.c
@@ -4,11 +4,21 @@
   int main(int argc, char const *argv[]){
 
     int i = 1;
+    int tab = 0;
+    int R = 0;
+
+    printf("digite o numero que deseja ver atabuada :");
+    scanf("%d",&tab);
 
     while (i <= 10)
     {
-        printf("ninho gostoso %d \n", i);
-        i++;
+        R = i * tab;
+        printf( "%d x %d = %d \n" ,tab,i,R);
+        printf("-----------\n");
+
+        i ++;
+
+        
     }
     
 
