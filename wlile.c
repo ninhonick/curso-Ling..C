c3a97f01ea888d5a4fec04211f75e8c1693b41bc
ninhonick <ninhosvs@gmail.com>
2025-01-22 00:37:18 -0300

    aula sobre while

---
+++ wlile.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+  #include <stdio.h>
+
+  int main(int argc, char const *argv[]){
+
+    int i = 1;
+
+    while (i <= 10)
+    {
+        printf("ninho gostoso %d \n", i);
+        i++;
+    }
+    
+
+
+     return 0;
+  }
+  
