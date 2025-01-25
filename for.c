26e53d727fa31bc2032e0e6028f9a8d7ccb1c8c5
ninhonick <ninhosvs@gmail.com>
2025-01-24 22:36:46 -0300

    aula de for

---
+++ for.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int i , count ;
+
+    printf("contar ate quanto boy? ------>>");
+    scanf("%d" ,&count);
+
+    for( i = 1; i<= count; i ){
+                              
+         printf("%d \n", i);
+
+         i += 2;
+}              
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
